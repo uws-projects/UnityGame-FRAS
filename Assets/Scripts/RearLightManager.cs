9b86326fbc2d2f0842ecdf438710e5e0af5645fd
tynutsu <tynutsu@yahoo.com>
2017-02-14 22:52:21 +0000

    -added trees

--- Assets/Scripts/RearLightManager.cs
+++ Assets/Scripts/RearLightManager.cs
@@ -1,27 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class RearLightManager : MonoBehaviour {
+namespace UnityStandardAssets.Vehicles.Car
+{
+    public class RearLightManager : MonoBehaviour
+    {
 
-    public Text TimeOfDay = null;
-    public int HourToSwitchOnLights = 17;
-    public int HourToSwitchOffLights = 8;
+        public Text TimeOfDay = null;
+        public Light left = null;
+        public Light right = null;
+        public CarController car; // reference to the car controller, must be dragged in inspector
+        public int HourToSwitchOnLights = 17;
+        public int HourToSwitchOffLights = 8;
 
-    private Renderer m_Renderer;
+        private Renderer m_Renderer;
 
-    // Use this for initialization
-    void Start () {
-        m_Renderer = GetComponent<Renderer>();
-    }
-	
-	// Update is called once per frame
-	void Update () {
+        // Use this for initialization
+        void Start()
+        {
+            m_Renderer = GetComponent<Renderer>();
+        }
 
-        int hour = 0;
-        int.TryParse(TimeOfDay.text.Split(":"[0])[0], out hour);
-        m_Renderer.enabled = !(hour > HourToSwitchOffLights && hour < HourToSwitchOnLights);
-
+        // Update is called once per frame
+        void Update()
+        {
+            int hour = 0;
+            int.TryParse(TimeOfDay.text.Split(":"[0])[0], out hour);
+            m_Renderer.enabled = !(hour > HourToSwitchOffLights && hour < HourToSwitchOnLights);
+            left.enabled = m_Renderer.enabled || car.BrakeInput > 0f;
+            right.enabled = left.enabled;
+        }
     }
 }
