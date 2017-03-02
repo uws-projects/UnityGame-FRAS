e38d4ddc15675e4e543cf50043aa9037ba628901
tynutsu <tynutsu@yahoo.com>
2017-03-02 05:34:14 +0000

    Added race logic Added directions Added finish screen (black)
    Signed-off-by: tynutsu <tynutsu@yahoo.com>

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -11,27 +11,34 @@ namespace UnityStandardAssets.Vehicles.Car
     {
         private CarController m_Car; // the car controller we want to use
         public Text speedText;
-
-
+        public bool raceFinished = false;
+        
         private void Awake()
         {
             // get the car controller
             m_Car = GetComponent<CarController>();
         }
 
+        public void finishRace()
+        {
+            raceFinished = true;
+        }
 
         private void FixedUpdate()
         {
-            // pass the input to the car!
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            if (!raceFinished)
+            {
+                // pass the input to the car!
+                float h = CrossPlatformInputManager.GetAxis("Horizontal");
+                float v = CrossPlatformInputManager.GetAxis("Vertical");
 #if !MOBILE_INPUT
-            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
-            m_Car.Move(h, v, v, handbrake);
-            speedText.text = ((int) m_Car.CurrentSpeed).ToString();
+                float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+                m_Car.Move(h, v, v, handbrake);
+                speedText.text = ((int)m_Car.CurrentSpeed).ToString();
 #else
             m_Car.Move(h, v, v, 0f);
 #endif
+            }
         }
     }
 }
