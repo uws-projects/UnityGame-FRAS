db9d5d0b0d1c5934b3299fff126c711170343b31
tynutsu <tynutsu@yahoo.com>
2017-03-09 04:54:34 +0000

    Added feedback form

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -14,4 +14,9 @@ public class ChangeScene : MonoBehaviour
         Application.LoadLevel(Application.loadedLevel - 1);
     }
 
+    public void SendFeedback()
+    {
+        Application.OpenURL("https://goo.gl/forms/x0zK5kF3OOs7Hf2C3");
+    }
+
 }
